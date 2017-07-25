1e22942f35233c246078a7e1504cbab7ab3bd2c3
hairmot <peter.colley@hotmail.com>
2017-07-25 13:06:19 +0100

    jquery

--- src/util/persistState.js
+++ src/util/persistState.js
@@ -1,4 +1,7 @@
 export default function(state, item = 'reactState') {
-		localStorage.setItem(item, JSON.stringify(state));
+	//	localStorage.setItem(item, JSON.stringify(state));
 		document.querySelector('[data-ajax]').value = JSON.stringify(state);
+		var formData = $('form').first().serialize() + '&NEXT.DUMMY.MENSYS.1=Next';
+			$.post($('form').first().attr('action'), formData, function() {
+		});
 	}
